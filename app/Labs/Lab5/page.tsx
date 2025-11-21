aac4c8f7c3c7bab7af50e292edd809eeedf084e7
aqeelzaman <aqeelzaman8@gmail.com>
2025-11-21 11:08:58 -0500

    a5 web api

--- app/Labs/Lab5/page.tsx
+++ app/Labs/Lab5/page.tsx
@@ -7,6 +7,7 @@ import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
 import WorkingWithObjects from "./WorkingWithObjects";
 import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
 const HTTP_SERVER = process.env.NEXT_PUBLIC_HTTP_SERVER;
+
 export default function Lab5() {
   return (
     <div id="wd-lab5">
